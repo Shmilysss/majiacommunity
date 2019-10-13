5b686ee3dfb38ab4710ac2810bb7209ec7142a8d
Shmilysss <18395585776@163.com>
2019-10-13 22:51:17 +0800

    client move to properties

--- src/main/java/shequ/wqy/community/controller/AuthorizeController.java
+++ src/main/java/shequ/wqy/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package shequ.wqy.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,18 +15,28 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
+
+
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("241bdde1a21536f5cb90");
-        accessTokenDTO.setClient_secret("a93b643ee74898a8ecc4ec1c430f4677489b0daf");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println("user="+user.getId());
+        System.out.println("user="+user.getName());
         return "index";
     }
 }
