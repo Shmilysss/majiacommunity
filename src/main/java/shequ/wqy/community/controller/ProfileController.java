f086c56da8bf2791554e73c7cec4a4b0d3ac6754
Shmilysss <18395585776@163.com>
2019-11-12 23:32:55 +0800

    add my question and huifu

---
+++ src/main/java/shequ/wqy/community/controller/ProfileController.java
@@ -0,0 +1,59 @@
+package shequ.wqy.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import shequ.wqy.community.mapper.UserMapper;
+import shequ.wqy.community.model.User;
+import shequ.wqy.community.service.QuestionService;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ProfileController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name="action") String action,
+                          HttpServletRequest request,
+                          Model model,
+                          @RequestParam(name = "page", defaultValue = "1") Integer page,
+                          @RequestParam(name = "size", defaultValue = "5") Integer size){
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        //表中存在当前token，返回登录界面，将user写进session和cookie
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        if(user == null){
+            return "redirect:/";
+        }
+        if("questions".equals(action)){
+            model.addAttribute("section","questions");
+            model.addAttribute("sectionName","我的提问");
+        }else if("replies".equals(action)){
+            model.addAttribute("section","replies");
+            model.addAttribute("sectionName","最新回复");
+        }
+        //questionService.list(user.getId(),page,size);
+        return "profile";
+    }
+}
