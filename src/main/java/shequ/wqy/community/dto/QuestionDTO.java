22fd7c3aaae7ca477573037951e9bb50096e6c1d
Shmilysss <18395585776@163.com>
2020-02-13 21:23:27 +0800

    修复bug

--- src/main/java/shequ/wqy/community/dto/QuestionDTO.java
+++ src/main/java/shequ/wqy/community/dto/QuestionDTO.java
@@ -5,7 +5,7 @@ import shequ.wqy.community.model.User;
 
 @Data
 public class QuestionDTO {
-    private Integer id;
+    private Long id;
     private String title;
     private String description;
     private String tag;
