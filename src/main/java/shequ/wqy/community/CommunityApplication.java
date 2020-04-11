7ea1e789c845fb4bda31da55d01afb3e3bf3bf7c
Shmilysss <18395585776@163.com>
2020-04-11 16:28:40 +0800

    补充定时器定时查询Question

--- src/main/java/shequ/wqy/community/CommunityApplication.java
+++ src/main/java/shequ/wqy/community/CommunityApplication.java
@@ -3,9 +3,11 @@ package shequ.wqy.community;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages = "shequ.wqy.community.mapper")
+@EnableScheduling
 public class CommunityApplication {
 
     public static void main(String[] args) {
--- src/main/java/shequ/wqy/community/schedule/HotTagTasks.java
+++ src/main/java/shequ/wqy/community/schedule/HotTagTasks.java
@@ -1,10 +1,17 @@
 package shequ.wqy.community.schedule;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import shequ.wqy.community.mapper.QuestionMapper;
+import shequ.wqy.community.model.Question;
+import shequ.wqy.community.model.QuestionExample;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Author: wanqiangying
@@ -14,10 +21,25 @@ import java.util.Date;
 @Component
 @Slf4j
 public class HotTagTasks {
-    @Scheduled(fixedRate = 60000)//1min
+
+    @Autowired
+    QuestionMapper questionMapper;
+
+    @Scheduled(fixedRate = 10000)
 //    @Scheduled(cron = "0 0 1 * * *") //每天凌晨1点
-    public void reportCurrentTime() {
+    public void hotTagSchedule() {
         //定时器任务，新的知识点
-        log.info("The time is now {}", new Date());
+        log.info("task begin {}", new Date());
+        int offset = 0;
+        int limit = 20;
+        List<Question> list = new ArrayList<>();
+        while (offset == 0 || list.size() == limit) {
+            list = questionMapper.selectByExampleWithRowbounds(new QuestionExample(), new RowBounds(offset, limit));
+            for (Question question : list) {
+                log.info("question ++ {} ++ {}",question.getId(),question.getTitle());
+            }
+            offset += limit;
+        }
+        log.info("task end {}", new Date());
     }
 }
